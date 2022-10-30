127baef3a15a366dc27250c1886beb88ac516e41
BeckyK <barimonsterbk@gmail.com>
2022-10-30 00:13:14 -0500

    updated code with formatting tweaks, fully functional

--- Sub StockTicker().vb
+++ Sub StockTicker().vb
@@ -3,10 +3,11 @@ Sub StockTicker()
     'create basic variables
     Dim TotVol As Double
     Dim Opn As Double
-    Dim Change As Double
+    Dim Cls As Double
+    Dim Chnge As Double
     Dim RowNum As Integer
    
-    'vars for greatest percent increase/decrease and total volume
+    'vars for greatest % increase/decrease and total volume
     Dim Gpi as Double
     Dim GpiTick as String
     Dim Gpd as Double
@@ -14,8 +15,10 @@ Sub StockTicker()
     Dim Gtv as Double
     Dim GtvTick as String
    
+   'loop through all worksheets
     For Each ws in Worksheets
-        'borrwed variable from census_pt1 project solved to get last row of worksheet
+
+        'borrwed variable from census_pt1_solved project to get last row of worksheet
         LastRow = ws.Cells(Rows.Count, 1).End(xlUp).Row
         'set var starting levels for each worksheet
         RowNum = 2
@@ -34,13 +37,13 @@ Sub StockTicker()
         ws.Cells(1, 15).Value = "Ticker"
         ws.Cells(1, 16).Value = "Value"
 
-        'loop thru all rows & check 1st column for ticker symbol
+        'loop through all rows
         For i = 2 To LastRow
 
             'add every column 7 for running total volume
             TotVol = TotVol + ws.Cells(i, 7).Value
 
-            'Simple If: get row 1 column 3 for opening (first row only) - store in var
+            'first row only, simple if - prev column 1 is different: get column 3 for opening and store in variable
             If ws.Cells(i - 1, 1).Value <> ws.Cells(i, 1).Value Then
                 Opn = ws.Cells(i, 3).Value
             End If
@@ -48,20 +51,25 @@ Sub StockTicker()
             'when next column 1 is different, get that row's column 6 for close and print out info in summary chart
             If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then
 
-                'print ticker symbol (column A) in column 9
+                'print ticker symbol (1st column) in column 9
                 ws.Cells(RowNum, 9).Value = ws.Cells(i, 1).Value
 
-                'change = close - open, print in column 10 & format color r/g
-                Change = ws.Cells(i, 6).Value - Opn
-                ws.Cells(RowNum, 10).Value = Change
-                If Change >= 0 Then
+                'change = close - open, print in column 10
+                Cls = ws.Cells(i, 6).Value
+                Chnge = Cls - Opn
+                ws.Cells(RowNum, 10).Value = Chnge
+                'fix Excel cutting off trailing zeros
+                ws.Range("J" & (RowNum - 1)).NumberFormat = "0.00"
+
+                'format color G/R for positive/negative
+                If Chnge >= 0 Then
                     ws.Cells(RowNum, 10).Interior.ColorIndex = 4
                 Else
                     ws.Cells(RowNum, 10).Interior.ColorIndex = 3
                 End If
 
                 'print percentage change & format in column 11
-                ws.Cells(RowNum, 11).Value = FormatPercent(Change/Opn)
+                ws.Cells(RowNum, 11).Value = FormatPercent(Chnge/Opn)
 
                 'check for greatest increase/decrease percentages
                 If ws.Cells(RowNum, 11).Value > Gpi Then
@@ -72,13 +80,10 @@ Sub StockTicker()
                     GpdTick = ws.Cells(i, 1).Value
                 End If
 
-                'print running total volume in column 12
+                'print total volume in column 12
                 ws.Cells(RowNum, 12).Value = TotVol
 
-                'increase row number for summary chart
-                RowNum = RowNum + 1
-
-                'check greatest total volume before zeroing out
+                'check greatest total volume before zeroing out var
                 If TotVol > Gtv Then
                     Gtv = TotVol
                     GtvTick = ws.Cells(i, 1).Value
@@ -86,6 +91,10 @@ Sub StockTicker()
 
                 'reset running total var
                 TotVol = 0
+
+                'increase row number for summary chart
+                RowNum = RowNum + 1
+
             End If
 
         Next i
@@ -97,6 +106,7 @@ Sub StockTicker()
         ws.Cells(3, 16).Value = FormatPercent(Gpd)
         ws.Cells(4, 15).Value = GtvTick
         ws.Cells(4, 16).Value = Gtv
+        ws.Range("I:P").EntireColumn.AutoFit
 
     Next ws
 
